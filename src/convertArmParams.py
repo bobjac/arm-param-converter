91c454997150e82444a8fb9669131cf50f696160
Bob Jacobs <bob.jacobs@outlook.com>
2022-11-28 19:42:02 -0500

    adding quotes to output

--- src/convertArmParams.py
+++ src/convertArmParams.py
@@ -12,7 +12,7 @@ def main():
     for key, value in data['parameters'].items():
         print(value['value'])
         param_value = value['value']
-        line = f'{key}={param_value}'
+        line = f'{key}="{param_value}"'
         env_file.write(f'{line}\n')
 
 if __name__ == "__main__":
