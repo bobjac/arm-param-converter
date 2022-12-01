ed60883fa29a5225c08e7cdad09811b8add38a24
Bob Jacobs <bob.jacobs@outlook.com>
2022-12-01 00:52:40 -0500

    updating generator

--- src/convertArmParams.py
+++ src/convertArmParams.py
@@ -7,13 +7,20 @@ def main():
     print(file_path)
     f = open(file_path)
     data = json.load(f)
+    generate_environment_file(data)
     
+    
+def generate_environment_file(json_data):
     env_file = open(".env", "a")
-    for key, value in data['parameters'].items():
+    for key, value in json_data['parameters'].items():
         print(value['value'])
         param_value = value['value']
         line = f'{key}="{param_value}"'
         env_file.write(f'{line}\n')
+
+def generate_powershell_comman(json_data):
+    env_file = open("powershellCmd.ps1", "a")
+    
 
 if __name__ == "__main__":
     main()
